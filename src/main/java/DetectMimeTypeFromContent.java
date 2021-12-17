08be5d5b19a5849815cb2a681ebe18625a05ac6b
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-12-17 22:58:17 +0530

    Update DetectMimeTypeFromContent.java

--- src/main/java/DetectMimeTypeFromContent.java
+++ src/main/java/DetectMimeTypeFromContent.java
@@ -2,6 +2,7 @@ import org.apache.tika.Tika;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class DetectMimeTypeFromContent {
@@ -9,10 +10,15 @@ public class DetectMimeTypeFromContent {
     public static void main(String args[]) throws IOException {
         Tika tika = new Tika();
 
-        Path path = new File("src/main/resources/test.csv").toPath();
+        Path path = new File("src/main/resources/test").toPath();
+        Path pathTxt = new File("src/main/resources/test.txt").toPath();
 
+        // Apache-Tika Returns mime type even when there is no file extension
         String fileContent = tika.detect(path.toFile());
         System.out.println(fileContent);
+
+        String mimeType = Files.probeContentType(pathTxt);
+        System.out.println(mimeType);
 
     }
 }
