aa212ed403b6f432f9543a1e1bbe40d00a678c00
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2022-02-20 15:30:15 +0530

    Update PrintingClassMethodNames.java

--- snippets/PrintingClassMethodNames.java
+++ snippets/PrintingClassMethodNames.java
@@ -17,10 +17,12 @@ public class App {
 // hello
 
 // Explanation
-// Arrays.stream(App.class.getMethods()) - Creates a stream for the supplied input
-// filter(method -> method.getDeclaringClass().equals(App.class)) - Since App class extends Object class by default it will iterate through methods names
-// in Object class . So check if class = App and return true 
+// Arrays.stream(App.class.getMethods()) - Creates a stream for the supplied array (Methods[])
+// filter(method -> method.getDeclaringClass().equals(App.class)) - Since App class extends Object class by default, it will iterate through method names
+// in Object class too . So check if class = App and return true 
 // peek(method->System.out.println(method.getName())) - peek()‘s Javadoc page says: “This method exists mainly to support debugging,
 // where you want to see the elements as they flow past a certain point in a pipeline“. This is an intermediate operation . 
 // collect(Collectors.toList()) - for peek method to work we should apply a terminal operation . Returns a list with App class method names 
-// The result isn't stored anywhere in the code here , but it is added to run the code supplied in peek method . Removing collect method will not run peek method 
+// The result isn't stored anywhere in the code here , but it is added to run the code supplied in peek method . Removing collect method will not run the code in peek method 
+
+// Source Stram API Peek source https://www.baeldung.com/java-streams-peek-api
